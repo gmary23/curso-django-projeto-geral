f0af0d197565fbb4a8f919e5da0164fc58b52ef4
Geila Martins <geila.estudos@gmail.com>
2023-10-14 13:40:53 -0300

    Exemplo de template temp

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from django.contrib import admin
-from recipes.views import  home, contato, sobre
+from recipes.views import home, contato, sobre
 
 urlpatterns = [
     path('', home),  # Home
