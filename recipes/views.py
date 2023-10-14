b2642eef17cd39353c0c6e801812f5ee0b6b6034
Geila Martins <geila.estudos@gmail.com>
2023-10-14 15:15:44 -0300

    criando pasta pages e partials e separando head e movendo home para pages

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 #HTTP REQUEST(cliente pede) 
 # recebeu o pedido do cliente
 def home(request): # foi criado essa função e colocou uma request
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Geila Martins',
     }) # direciona para a home.html que fica dentro de templates do app recipes
