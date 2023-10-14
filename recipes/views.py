8e5e2ce981ed650027ba823407e16bf866d9692b
Geila Martins <geila.estudos@gmail.com>
2023-10-14 14:15:36 -0300

    Removido contato e sobre da views - urls e import

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import path # aqui fala para importa do todas as rotas e funções da urls do projeto
-
 
 # Create your views here.
 #HTTP REQUEST(cliente pede) 
@@ -10,9 +7,3 @@ def home(request): # foi criado essa função e colocou uma request
     return render(request, 'recipes/home.html', context={
         'name': 'Geila Martins',
     }) # direciona para a home.html que fica dentro de templates do app recipes
-
-def contato(request): # foi criado essa função e colocou uma request
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request): # foi criado essa função e colocou uma request
-    return HttpResponse('SOBRE AS RECEITAS')
