b607c659c79468fd6b20db71ff65d8232ec99dd0
Geila Martins <geila.estudos@gmail.com>
2023-10-25 17:20:11 -0300

    modificações no css e páginas html

--- projeto/urls.py
+++ projeto/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),# recebe do arquivo e a url que vai incluir
-   # path('recipes/', include('recipes.urls')),
+    path('', include('recipes.urls')),
 ]
