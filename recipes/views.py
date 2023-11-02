dbb5d31112befe2af7a4a8877809732dcd1dfef6
Geila Martins <geila.estudos@gmail.com>
2023-11-02 10:54:10 -0300

    alteração nas views, settings - criação do footer

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-#HTTP REQUEST(cliente pede) 
+# HTTP REQUEST(cliente pede)
 # recebeu o pedido do cliente
-def home(request): # foi criado essa função e colocou uma request
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Geila Martins',
-    }) # direciona para a home.html que fica dentro de templates do app recipes
+def home(request):  # foi criado essa função e colocou uma request
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "name": "Geila Martins",
+        },
+    )  # direciona para a home.html que fica dentro de templates do app recipes
+
+
+def recipe(request, id):
+    return render(request, "recipes/pages/home.html", context={"name": "Geila Azevedo"})
