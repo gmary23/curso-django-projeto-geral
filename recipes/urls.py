1ee523d26960fa119309811a867ce36fcc16f443
Geila Martins <geila.estudos@gmail.com>
2023-11-23 07:42:12 -0300

    Colocando informações reais no models

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,6 @@ app_name = "recipes"
 
 urlpatterns = [
     path("", views.home, name="home"),  # Home - name é usado para dar um nome único
+    path("recipes/category/<int:category_id>/", views.category, name="category"),
     path("recipes/<int:id>/", views.recipe, name="recipe"),  # Recipe
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by("-id")  # aplicativo
     return render(
         request,
         "recipes/pages/home.html",
         context={
-            "recipes": [make_recipe() for _ in range(12)],
+            "recipes": recipes,  # nome das receitas que tem cadastradas
+        },
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category__id).order_by(
+        "-id"
+    )  # aplicativo
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,  # nome das receitas que tem cadastradas
         },
     )
 
