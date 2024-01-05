583f09a7296aed75652b83617f2b9bc9ea0abe05
Geila Martins <geila.estudos@gmail.com>
2024-01-05 10:31:35 -0300

    iniciando testes no django

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse 
 
-
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+class RecipeURLsTest(TestCase):  # noqa
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
