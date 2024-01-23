b191d3cd18632299274f4e74ca358378ab014b96
Geila Martins <geila.estudos@gmail.com>
2024-01-23 09:53:07 -0300

    Aula 77 - Separando os testes em um package chamado tests __init__.py

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse 
+from django.urls import resolve, reverse
+
+from recipes import views
+
 
 class RecipeURLsTest(TestCase):  # noqa
     def test_recipe_home_url_is_correct(self):
@@ -15,3 +19,19 @@ class RecipeURLsTest(TestCase):  # noqa
         self.assertEqual(url, '/recipes/1/')
 
 
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, view.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, view.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
