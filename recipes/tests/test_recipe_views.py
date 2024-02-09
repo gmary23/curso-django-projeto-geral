7b2a82ae294a6452f18f4a80e7be81b5c3b75df7
Geila Martins <geila.estudos@gmail.com>
2024-02-09 09:12:20 -0300

    Aula 81 - Criando um teste da home

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -41,6 +42,32 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here 🥲</h1>',
             response.content.decode('utf-8')
         )
+    def teste_recipe_home_template_loads_recipes(self): # testa se o templete da home carrega as receitas
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit ='Minutos', 
+            servings = 5,
+            servings_unit ='Porções', 
+            preparation_steps ='Recipe Prepatation Steps', 
+            preparation_steps_is_html=False
+            is_published=True
+        )
+        assert 1 == 1
+
     def teste_recipe_home_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
